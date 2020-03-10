c974e7a5f8d0e7f2ec9cfe95359824627bdf7d36
Giorgos Neokleous <neokleoys2005@gmail.com>
2020-03-10 20:41:11 +0000

    added layout and empty click listeners

--- app/src/main/java/com/giorgosneokleous/fullscreenintentexample/MainActivity.kt
+++ app/src/main/java/com/giorgosneokleous/fullscreenintentexample/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.giorgosneokleous.fullscreenintentexample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.showFullScreenIntentButton).setOnClickListener {
+            showFullScreenIntent()
+        }
+
+        findViewById<Button>(R.id.showFullScreenIntentWithDelayButton).setOnClickListener {
+            scheduleFullScreenIntent()
+        }
+    }
+
+    private fun scheduleFullScreenIntent() {
+        TODO("Not yet implemented")
+    }
+
+    private fun showFullScreenIntent() {
+        TODO("Not yet implemented")
     }
 }
