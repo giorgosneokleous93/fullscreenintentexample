ca51bc5dc84920cbcc8d21b47c9b49ae87a7660d
Giorgos Neokleous <neokleoys2005@gmail.com>
2020-03-20 12:14:44 +0000

    renamed the extension function of turning screen on and keyguard off to be correct semantically

--- app/src/main/java/com/giorgosneokleous/fullscreenintentexample/ActivityUtils.kt
+++ app/src/main/java/com/giorgosneokleous/fullscreenintentexample/ActivityUtils.kt
@@ -30,7 +30,7 @@ import android.content.Context
 import android.os.Build
 import android.view.WindowManager
 
-fun Activity.turnScreenAndKeyguardOn() {
+fun Activity.turnScreenOnAndKeyguardOff() {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
         setShowWhenLocked(true)
         setTurnScreenOn(true)
@@ -43,7 +43,19 @@ fun Activity.turnScreenAndKeyguardOn() {
 
     with(getSystemService(Context.KEYGUARD_SERVICE) as KeyguardManager) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            requestDismissKeyguard(this@turnScreenAndKeyguardOn, null)
+            requestDismissKeyguard(this@turnScreenOnAndKeyguardOff, null)
         }
+    }
+}
+
+fun Activity.turnScreenOffAndKeyguardOn() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O_MR1) {
+        setShowWhenLocked(false)
+        setTurnScreenOn(false)
+    } else {
+        window.clearFlags(
+            WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
+                    or WindowManager.LayoutParams.FLAG_ALLOW_LOCK_WHILE_SCREEN_ON
+        )
     }
 }
--- app/src/main/java/com/giorgosneokleous/fullscreenintentexample/LockScreenActivity.kt
+++ app/src/main/java/com/giorgosneokleous/fullscreenintentexample/LockScreenActivity.kt
@@ -31,6 +31,11 @@ class LockScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lock_screen)
-        turnScreenAndKeyguardOn()
+        turnScreenOnAndKeyguardOff()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        turnScreenOffAndKeyguardOn()
     }
 }
